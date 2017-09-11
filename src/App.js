02bfea0065b995cdaebe74be2b605a23f78c758b
juanmottesi <juannpm159@gmail.com>
2017-09-10 23:20:16 -0300

    nitial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Header/>
+        <Body/>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render(){
+    return (
+      <h3> Hola Mundo! </h3>
+    );
+  }
+}
+
+class Body extends Component {
+  render(){
+    return(
+      <div id="accordion" role="tablist">
+        <Course courseName="Matematica 1" date="Lunes 15hs - 18hs" href="#matematica1" id="matematica1"/>
+        <Course courseName="Matematica 2" date={["Lunes 18hs - 22hs", "Martes 18hs - 22hs"]} href="#matematica2" id="matematica2"/>
+      </div>
+    );
+  }
+}
+
+class Course extends Component {
+  render(){
+    return(
+      <div className="card">
+        <div className="card-header" role="tab" id="headingOne">
+          <h5 className="mb-0">
+            <a data-toggle="collapse" href={this.props.href} aria-expanded="true" aria-controls="collapseOne">
+              <p>{this.props.courseName}</p>
+            </a>
+          </h5>
+        </div>
+        <div id={this.props.id} className="collapse" role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion">
+          <div className="card-body">
+            <p>{this.props.date}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default App;
